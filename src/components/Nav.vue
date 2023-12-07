1312665a6e20e88cc3c5aafd098233b8acb0a59f
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2023-12-07 21:50:12 +0100

    updated

---
+++ src/components/Nav.vue
@@ -0,0 +1,53 @@
+<script>
+export default{
+  data(){
+    return {
+      scrollToPage:""
+    }
+  },
+  methods: {
+    scrollToAbout() {
+      this.scrollToPage="about-me",
+      this.$emit("scrollToAbout", this.scrollToPage);
+    },
+  },
+}
+</script>
+
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary shadow ">
+        <div class="container-fluid">
+          <a class="navbar-brand ms-5 text-style" href="#"
+            >Ruhi Ramakant Ambelkar</a
+          >
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <ul class="navbar-nav justify-content-end me-5">
+            <li class="nav-item p-3">
+              <a class="nav-link" href="#" >Home</a>
+            </li>
+            <li class="nav-item p-3">
+              <a class="nav-link" href="#">Projects</a>
+            </li>
+            <li class="nav-item p-3">
+              <a class="nav-link" href="#" @click="scrollToAbout">About</a>
+            </li>
+            <li class="nav-item p-3">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+</template>
---
+++ src/views/Contact.vue
@@ -0,0 +1,74 @@
+<script>
+import  Email  from "@/assets/smtp/smtp.js";
+
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      message: "",
+      subject:"",
+    };
+  },
+  methods: {
+    sendMail() {
+        console.log("email:",this.email,"Name:",this.name,"subj:",this.subject,"msg:",this.message);
+      Email.send({
+        SecureToken: "db03cdbd-940e-4cd9-ab60-8b5cb816888e",
+        To: "ruhiambelkar7@gmail.com",
+        From: this.email,
+        name:this.name,
+        Subject:this.subject,
+        Body: this.message,
+      }).then((message) => alert(message));
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="row my-5">
+    <div class="col-md-6 offset-md-3">
+      <div id="contact-heading">
+        <h1 class="display-1 text-center">Contact Me</h1>
+      </div>
+      <form class="border rounded p-5 shadow">
+        <div class="mb-3">
+          <label for="name" class="form-label">Name</label>
+          <input type="text" id="name" class="form-control" v-model="name" />
+        </div>
+        <div class="mb-3">
+          <label for="subject" class="form-label">Subject</label>
+          <input type="text" id="subject" class="form-control" v-model="subject" />
+        </div>
+        <div class="mb-3">
+          <label for="email" class="form-label">Email</label>
+          <input type="email" id="email" class="form-control" v-model="email" />
+        </div>
+        <div class="mb-3">
+          <label for="name" class="form-label">Messase</label>
+          <textarea rows="4" id="name" class="form-control" v-model="message" />
+        </div>
+        <div class="d-flex justify-content-center mt-4">
+          <input class="btn btn-primary" type="button" value="Submit" @click="sendMail()" />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<style scoped>
+#contact-heading {
+  color: rgb(4, 46, 49);
+}
+
+form {
+  color: rgb(4, 46, 49);
+  background-color: rgb(148, 205, 209) !important ;
+}
+
+.btn {
+  background-color: rgb(4, 46, 49);
+  border-color: rgb(4, 46, 49);
+  width: 200px;
+}
+</style>
